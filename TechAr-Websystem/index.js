e443c821e0d13a81649a739f3e270fa0e0fea92e
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-02-01 14:50:10 +0530

    Added initial files for dashboard

--- TechAr-Websystem/index.js
+++ TechAr-Websystem/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path')
@@ -23,6 +23,19 @@ app.get('/sign-in', (req, res) => {
 app.get('/admin-panel', (req, res, next) => {
     res.render('admin-login', {});
 });
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard',{
+        //TODO Remember We need to give it this data through a database, Currently managing these properties through intiial design thought of.
+        length:0,
+        mail:'namankalrabhiwani54@gmail.com',
+        queries:[],
+        queries_length:0,
+
+    })
+  });
+  
+  ///////////////
   
 app.listen(port, () => {
     console.log('Server Started at ' + port);
