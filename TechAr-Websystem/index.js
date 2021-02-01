c5bdc07d67bf49c3934ee5698b29849f6bbe4246
nlok5923 <nlokhande5923@gmail.com>
2021-02-01 14:25:00 +0530

    added page were admin can add instructors

--- TechAr-Websystem/index.js
+++ TechAr-Websystem/index.js
@@ -31,13 +31,13 @@ app.get("/",(req,res)=>{
 app.get('/sign-in', (req, res) => {
     res.render('sign-in', {});
 });
-app.get("/generate",(req,res)=>{
+app.get("/dashboard/generate",(req,res)=>{
     res.render('Generator',{});
 })
 app.get('/admin-panel', (req, res, next) => {
     res.render('admin-login', {});
 });
-  
+
 app.listen(port, () => {
     console.log('Server Started at ' + port);
 });
