31b36b3d925bb94b47bb9f06a6b4b1ed10a5e444
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-02-02 13:40:32 +0530

    Add Codes for styling and Get request for our page

--- TechAr-Websystem/index.js
+++ TechAr-Websystem/index.js
@@ -48,7 +48,9 @@ app.get('/admin-panel', (req, res, next) => {
 app.get("/team",(req,res)=>{
   res.render('Team',{})
 });
-
+app.get("/about",(req,res)=>{
+  res.render('why_us',{})
+})
 
 // this post route will take data provided by admin and save it as a instructor credentials 
 app.post('/admin-panel', async (req, res, next) => {
